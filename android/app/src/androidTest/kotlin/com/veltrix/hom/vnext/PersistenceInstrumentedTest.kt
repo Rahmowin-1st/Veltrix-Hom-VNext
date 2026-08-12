cddf44b9f9577ca2873c27b1a617c1da6ee8b9d9
Rahmowin-1st <shahbozrahmonov7707@gmail.com>
2026-08-12 13:51:51 +0500

    fix: import Compose onAllNodes test API

--- android/app/src/androidTest/kotlin/com/veltrix/hom/vnext/PersistenceInstrumentedTest.kt
+++ android/app/src/androidTest/kotlin/com/veltrix/hom/vnext/PersistenceInstrumentedTest.kt
@@ -3,6 +3,7 @@ package com.veltrix.hom.vnext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.onAllNodes
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
