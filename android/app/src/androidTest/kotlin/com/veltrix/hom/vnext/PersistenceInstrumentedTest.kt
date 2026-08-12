59112a3d8242223bd5c2e6ab9c12c36edae90fd0
Rahmowin-1st <shahbozrahmonov7707@gmail.com>
2026-08-12 13:31:41 +0500

    test: use Compose rule onAllNodes scope API

--- android/app/src/androidTest/kotlin/com/veltrix/hom/vnext/PersistenceInstrumentedTest.kt
+++ android/app/src/androidTest/kotlin/com/veltrix/hom/vnext/PersistenceInstrumentedTest.kt
@@ -3,7 +3,6 @@ package com.veltrix.hom.vnext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.hasText
-import androidx.compose.ui.test.onAllNodes
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
