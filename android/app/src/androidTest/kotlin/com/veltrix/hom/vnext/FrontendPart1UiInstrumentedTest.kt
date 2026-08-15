ea2380fb9130c34640080f553699c27d7b19d6a1
Rahmowin-1st <shahbozrahmonov7707@gmail.com>
2026-08-15 09:35:13 +0500

    test(frontend): use SemanticsNodeInteraction assertExists member

--- android/app/src/androidTest/kotlin/com/veltrix/hom/vnext/FrontendPart1UiInstrumentedTest.kt
+++ android/app/src/androidTest/kotlin/com/veltrix/hom/vnext/FrontendPart1UiInstrumentedTest.kt
@@ -1,6 +1,5 @@
 package com.veltrix.hom.vnext
 
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
