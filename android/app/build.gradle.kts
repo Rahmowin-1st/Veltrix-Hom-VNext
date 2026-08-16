0068fbc42e80a55740f0f06a1b7855ceb913d382
Rahmowin-1st <shahbozrahmonov7707@gmail.com>
2026-08-16 13:58:57 +0500

    test(frontend): add stable JankStats performance instrumentation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,5 +42,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.metrics:metrics-performance:1.0.0")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
