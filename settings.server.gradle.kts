564330d849bfd3ea18ec077b4aa12656021f2328
Rahmowin-1st <shahbozrahmonov7707@gmail.com>
2026-08-21 12:29:39 +0500

    deploy: add server-only Gradle settings for live vNext runtime

---
+++ settings.server.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Veltrix-Hom-VNext-Server"
+include(":core")
+include(":server")
